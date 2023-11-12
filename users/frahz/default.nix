f0e7b98b99b353a619734175e0d36e98462dc03e
frahz <abreufranklin589@gmail.com>
2023-11-11 19:19:44 -0800

    add `frahz` user to docker group

--- users/frahz/default.nix
+++ users/frahz/default.nix
@@ -5,7 +5,7 @@ in
 {
     users.users.${username} = {
         isNormalUser = true;
-        extraGroups = [ "wheel" "networkmanager" "audio" "video" "lp" ];
+        extraGroups = [ "wheel" "networkmanager" "audio" "video" "lp" "docker" ];
         shell = pkgs.zsh;
         description = "frahz";
         uid = 1000;
