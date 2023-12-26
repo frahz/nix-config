9b0db7640138b246dfb2da680e00f3f87bc2f912
frahz <abreufranklin589@gmail.com>
2023-12-26 13:01:43 -0800

    revert last commit

--- modules/containers/pihole.nix
+++ modules/containers/pihole.nix
@@ -18,9 +18,6 @@
             "53:53/udp"
             "8053:80/tcp"
         ];
-        extraOptions = [
-            "--cap-add=NET_ADMIN"
-            "--network=host"
-        ];
+        extraOptions = [ "--cap-add=NET_ADMIN" ];
     };
 }
