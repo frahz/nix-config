829c65972d03ae747688b1c8dc68f68081d628df
frahz <abreufranklin589@gmail.com>
2024-02-18 16:01:05 -0800

    home: update neovim config

--- home/neovim/config/treesitter.lua
+++ home/neovim/config/treesitter.lua
@@ -1,22 +1,6 @@
 local configs = require("nvim-treesitter.configs")
 
 configs.setup({
-    ensure_installed = {
-        "python",
-        "lua",
-        "rust",
-        "cpp",
-        "c",
-        "bash",
-        "java",
-        "css",
-        "html",
-        "typescript",
-        "javascript",
-        "toml",
-        "yaml",
-        "nix"
-    },
     highlight = {
         enable = true,
         -- disable = { "c" },
