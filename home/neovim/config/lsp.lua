e0fbcdaa270aee0257baab79bcce249cd9636b3c
frahz <abreufranklin589@gmail.com>
2024-02-18 16:13:12 -0800

    home: update neovim config

--- home/neovim/config/lsp.lua
+++ home/neovim/config/lsp.lua
@@ -62,7 +62,7 @@ nvim_lsp.lua_ls.setup({
                 globals = { "vim" },
             },
             workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
+                library = {},
                 checkThirdParty = false,
             },
             telemetry = {
