0fa974b252af6b40c51ee0daf51184c9472587b7
frahz <abreufranklin589@gmail.com>
2024-04-10 20:26:36 -0700

    home: git delta

--- home/git/default.nix
+++ home/git/default.nix
@@ -6,8 +6,9 @@
   path = "delta/themes/mocha.gitconfig";
 in {
   xdg.configFile = {
+    # TODO: update this to use tip
     ${path}.source = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/catppuccin/delta/main/themes/mocha.gitconfig";
+      url = "https://raw.githubusercontent.com/catppuccin/delta/46ae4c14f976c8d4feb946ac7d35db964aa07f79/themes/mocha.gitconfig";
       hash = "sha256-I2sD5TEYjnvvms5JWFEM/wW+4V3ofTOwaRBhdQko0CM=";
     };
   };
