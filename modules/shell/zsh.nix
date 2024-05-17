82707161b348e20af67e44af62c9ebfa7be0050c
frahz <me@frahz.dev>
2024-05-16 18:50:13 -0700

    speed up zsh

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -1,14 +1,6 @@
 _: {
   programs.zsh = {
     enable = true;
-    enableCompletion = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-    ohMyZsh = {
-      enable = true;
-      plugins = ["git" "tmux"];
-      theme = "lambda"; # change to my own theme later on
-    };
     shellAliases = {
       vim = "nvim";
     };
