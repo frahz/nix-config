c6fc4f3a148b43ae18a2e0d87e27034e65eaef3f
frahz <me@frahz.dev>
2024-05-16 18:08:37 -0700

    home: add zsh stuff

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -1,4 +1,8 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   xdg.configFile."omz/themes/frahz.zsh-theme" = {
     source = ./frahz.zsh-theme;
   };
@@ -15,6 +19,13 @@
       theme = "frahz";
     };
 
+    plugins = [
+      {
+        name = "fzf-tab";
+        src = "${pkgs.zsh-fzf-tab}/share/fzf-tab";
+      }
+    ];
+
     syntaxHighlighting = {
       enable = true;
     };
@@ -22,5 +33,12 @@
     shellAliases = {
       vim = "nvim";
     };
+
+    initExtra = ''
+      # force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+      zstyle ':completion:*' menu no
+      # preview directory's content with exa when completing cd
+      zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls -hAF --color=always "$realpath"'
+    '';
   };
 }
