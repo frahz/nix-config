a06dd371e53a7edd6a263a73f8243fd4412b8623
frahz <abreufranklin589@gmail.com>
2024-05-25 12:48:32 -0700

    neovim: update stuff

--- home/neovim/config/lsp_handlers.lua
+++ home/neovim/config/lsp_handlers.lua
@@ -1,21 +1,15 @@
 local M = {}
 
 M.setup = function()
-	local signs = {
-		{ name = "DiagnosticSignError", text = "" },
-		{ name = "DiagnosticSignWarn", text = "" },
-		{ name = "DiagnosticSignHint", text = "" },
-		{ name = "DiagnosticSignInfo", text = "" },
-	}
-
-	for _, sign in ipairs(signs) do
-		vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-	end
-
 	local config = {
 		virtual_text = true,
 		signs = {
-			active = signs,
+			text = {
+				[vim.diagnostic.severity.ERROR] = "",
+				[vim.diagnostic.severity.WARN] = "",
+				[vim.diagnostic.severity.INFO] = "",
+				[vim.diagnostic.severity.HINT] = "",
+			},
 		},
 		update_in_insert = false,
 		underline = true,
@@ -42,4 +36,3 @@ M.setup = function()
 end
 
 return M
-
--- home/neovim/config/theme.lua
+++ home/neovim/config/theme.lua
@@ -3,44 +3,11 @@ local catppuccin = require("catppuccin");
 catppuccin.setup({
     flavour = "mocha",
     no_italic = true;
-    styles = {
-        comments = {},
-        conditionals = {},
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-    },
     integrations = {
         treesitter = true,
         cmp = true,
-        native_lsp = {
-            enabled = true,
-            virtual_text = {
-                errors = {},
-                hints = {},
-                warnings = {},
-                information = {},
-            },
-        },
         mason = true,
     },
-    custom_highlights = function()
-        return {
-            ["@parameter"] = { style = {} },
-            ["@namespace"] = { style = {} },
-            ["@text.emphasis"] = { style = {} },
-            ["@text.literal"] = { style = {} },
-            ["@text.uri"] = { style =  { "underline" } },
-            ["@tag.attribute"] = { style = {} },
-        }
-    end
 })
 
 vim.api.nvim_command("colorscheme catppuccin")
