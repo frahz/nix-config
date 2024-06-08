ce317da90ab14a0e43531516eec657be8d14403b
frahz <me@frahz.dev>
2024-06-07 19:55:25 -0700

    home: update hash for btop

--- home/terminal/btop.nix
+++ home/terminal/btop.nix
@@ -7,7 +7,7 @@
   xdg.configFile = {
     "btop/themes/catppuccin_mocha.theme".source = pkgs.fetchurl {
       url = "https://raw.githubusercontent.com/catppuccin/btop/main/themes/catppuccin_mocha.theme";
-      hash = "sha256-TeaxAadm04h4c55aXYUdzHtFc7pb12e0wQmCjSymuug=";
+      hash = "sha256-THRpq5vaKCwf9gaso3ycC4TNDLZtBB5Ofh/tOXkfRkQ=";
     };
   };
 }
