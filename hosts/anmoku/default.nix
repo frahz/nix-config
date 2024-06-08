530cb78eaa5fa055a738b2d792129b45e22b71a7
frahz <abreufranklin589@gmail.com>
2024-06-07 20:52:00 -0700

    hyprland stuff

--- hosts/anmoku/default.nix
+++ hosts/anmoku/default.nix
@@ -1,5 +1,10 @@
-{pkgs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   imports = [
+    inputs.hyprland.nixosModules.default
     ./hardware-configuration.nix
     ../../modules/fonts.nix
     ../../modules/wayland
@@ -19,6 +24,12 @@
     kernelPackages = pkgs.linuxPackages_latest;
   };
 
+  programs.hyprland = {
+    enable = true;
+    #  finalPackage = inputs.hyprland.packages.${pkgs.system}.default;
+    portalPackage = inputs.xdg-portal-hyprland.packages.${pkgs.system}.default;
+  };
+
   services = {
     pipewire = {
       enable = true;
@@ -26,7 +37,13 @@
       alsa.support32Bit = true;
       pulse.enable = true;
     };
+    security = {
+      rtkit.enable = true;
+      polkit.enable = true;
+    };
   };
+
+  hardware.pulseaudio.enable = false;
 
   networking.networkmanager.enable = true;
 }
