beecdaf9da617d7046c9d8a29516ac232c343883
frahz <abreufranklin589@gmail.com>
2024-06-07 21:37:17 -0700

    desktop stuff

--- modules/wayland/default.nix
+++ modules/wayland/default.nix
@@ -16,9 +16,13 @@
       # wlr-randr
     ];
     sessionVariables = {
+      __GL_GSYNC_ALLOWED = "0";
+      __GL_VRR_ALLOWED = "0";
+      WLR_DRM_NO_ATOMIC = "1";
       XDG_SESSION_TYPE = "wayland";
       QT_QPA_PLATFORM = "wayland";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+      GDK_BACKEND = "wayland";
       MOZ_ENABLE_WAYLAND = "1";
       # WLR_BACKEND = "vulkan";
       # WLR_RENDERER = "vulkan";
