ae8ed2ece53c1ded028fddedbf71bf67031a26a3
frahz <abreufranklin589@gmail.com>
2024-06-08 14:10:17 -0700

    desktop: alacritty settings

--- home/desktop/alacritty.nix
+++ home/desktop/alacritty.nix
@@ -1,12 +1,31 @@
-_: {
+{pkgs, ...}: {
   programs.alacritty = {
     enable = true;
     settings = {
+      window = {
+        decorations = "none";
+        dynamic_padding = true;
+        padding = {
+          x = 5;
+          y = 5;
+        };
+        startup_mode = "Maximized";
+      };
+
+      scrolling.history = 10000;
+
       font = {
-        normal.family = "Iosevka NF";
-        bold = {style = "Bold";};
+        normal.family = "Iosevka Term NF";
+        bold.family = "Iosevka Term NF";
         size = 11;
       };
+
+      imports = [
+        (pkgs.fetchurl {
+          url = "https://raw.githubusercontent.com/catppuccin/alacritty/3c808cbb4f9c87be43ba5241bc57373c793d2f17/catppuccin-mocha.yml";
+          hash = "sha256-28Tvtf8A/rx40J9PKXH6NL3h/OKfn3TQT1K9G8iWCkM=";
+        })
+      ];
     };
   };
 }
