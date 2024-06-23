9bb30080b20b8fc1f88d4601dbde35961c875b8a
frahz <me@frahz.dev>
2024-06-23 00:59:49 -0700

    desktop: firefox policies/settings

--- home/desktop/programs/firefox.nix
+++ home/desktop/programs/firefox.nix
@@ -3,13 +3,26 @@
     enable = true;
     policies = {
       DisablePocket = true;
-      OfferToSaveLogins = true;
       PasswordManagerEnable = false;
+      DisplayBookmarksToolbar = true;
+      DontCheckDefaultBrowser = true;
+      OfferToSaveLogins = false;
+      NoDefaultBookmarks = true;
+      DNSOverHTTPS = {
+        Enabled = true;
+      };
     };
     profiles.frahz = {
       id = 0;
       isDefault = true;
       name = "frahz";
+      settings = {
+        # 0 => blank page
+        # 1 => your home page(s) {default}
+        # 2 => the last page viewed in Firefox
+        # 3 => previous session windows and tabs
+        "browser.startup.page" = 3;
+      };
     };
   };
 }
