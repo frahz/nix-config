52677a17c348a19237a9322e3b284ec91bd8b33b
frahz <me@frahz.dev>
2024-08-28 21:13:16 -0700

    add new ssh key

--- users/frahz/default.nix
+++ users/frahz/default.nix
@@ -14,6 +14,7 @@ in {
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ZRCt+Fhx6tVAih57zCNgv8E/THbv8eAl6hXEnmS5/"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILKyBGpzn7xsMi9oqsOUHsWrTiTsH47nj3eW1cFK/JQ+ me@frahz.dev"
     ];
   };
 }
