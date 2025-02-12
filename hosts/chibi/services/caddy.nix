a0b160d3c3c00e4d96831f9f69fa1e51e5799a25
frahz <me@frahz.dev>
2025-02-11 22:32:31 -0800

    chibi: update caddy

--- hosts/chibi/services/caddy.nix
+++ hosts/chibi/services/caddy.nix
@@ -82,7 +82,7 @@ in {
     email = "me@frahz.dev";
     package = pkgs.caddy.withPlugins {
       plugins = ["github.com/caddy-dns/porkbun@v0.2.1"];
-      hash = "sha256-5rfdWHT2ah5THFKjcSoN+aTLhnjQYbNFQxQTfXB439I=";
+      hash = "sha256-ayd1WnjBjQOIXtiCkR/aWML2Nc4eRyuTugsjHXOU5uQ=";
     };
     globalConfig = ''
       acme_dns porkbun {
