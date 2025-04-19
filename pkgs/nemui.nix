730c6039536fc1be234305a4ee876863a05a742e
frahz <me@frahz.dev>
2025-04-19 12:35:48 -0700

    inari: update nemui

--- pkgs/nemui.nix
+++ pkgs/nemui.nix
@@ -6,16 +6,16 @@
 }:
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "nemui";
-  version = "0.2.0";
+  version = "0.3.0";
 
   src = fetchFromGitHub {
     owner = "frahz";
     repo = "nemui";
     rev = "v${version}";
-    hash = "sha256-zvLHHvEAj1uA7BOr+As7T5BfHtgTVwZBEm7cwHA0ORo=";
+    hash = "sha256-rbKSO0j0Fr3WcXzUSyYgcIqiQqgTg2Mzla1Et62HVQ0=";
   };
 
-  cargoHash = "sha256-4Bd9Ja07TcJmDQzkwUfX95BGqQbI4stDrO7HRGP+y5Y=";
+  cargoHash = "sha256-zP6ABfX8Jveei/v/2JSo98mvqiRs2kWObpa7jVjNimg=";
 
   meta = with lib; {
     description = "This utility is meant to run in the your main server. Once it receives a specific byte over it will put the server to sleep.";
