351ca1865d799af4376f6cee6e02926b921b1a92
frahz <me@frahz.dev>
2025-05-04 12:10:30 -0700

    home: use catppuccin for bat/delta

--- home/terminal/bat.nix
+++ home/terminal/bat.nix
@@ -1,21 +1,6 @@
-{pkgs, ...}: {
+_: {
   programs.bat = {
     enable = true;
-    config = {
-      theme = "Catppuccin-mocha";
-    };
-    themes = let
-      src = pkgs.fetchFromGitHub {
-        owner = "catppuccin";
-        repo = "bat";
-        rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
-        hash = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
-      };
-    in {
-      Catppuccin-mocha = {
-        inherit src;
-        file = "Catppuccin-mocha.tmTheme";
-      };
-    };
   };
+  catppuccin.bat.enable = true;
 }
--- home/terminal/git/default.nix
+++ home/terminal/git/default.nix
@@ -1,33 +1,12 @@
-{
-  config,
-  pkgs,
-  ...
-}: let
-  path = "delta/themes/mocha.gitconfig";
-in {
-  xdg.configFile = {
-    # TODO: update this to use tip
-    ${path}.source = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/catppuccin/delta/46ae4c14f976c8d4feb946ac7d35db964aa07f79/themes/mocha.gitconfig";
-      hash = "sha256-I2sD5TEYjnvvms5JWFEM/wW+4V3ofTOwaRBhdQko0CM=";
-    };
-  };
-
+_: {
   programs.git = {
     enable = true;
     userName = "frahz";
     userEmail = "me@frahz.dev";
 
-    includes = [
-      {
-        path = "${config.xdg.configHome}/${path}";
-      }
-    ];
-
     delta = {
       enable = true;
       options = {
-        features = "catppuccin-mocha";
         navigate = true;
         light = false;
         line-numbers = true;
@@ -43,4 +22,5 @@ in {
       init.defaultBranch = "main";
     };
   };
+  catppuccin.delta.enable = true;
 }
