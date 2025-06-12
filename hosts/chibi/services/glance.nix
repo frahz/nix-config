5e4a3bca2600a69517363901c87753b2dbcff0fa
frahz <me@frahz.dev>
2025-06-11 19:37:55 -0700

    chibi: add new widget to glance

--- hosts/chibi/services/glance.nix
+++ hosts/chibi/services/glance.nix
@@ -49,6 +49,141 @@ in {
                   password = "\${ADGUARD_PASSWORD}";
                 }
                 # TODO: add media-requests widget here: https://github.com/glanceapp/glance/pull/345
+                {
+                  type = "custom-api";
+                  title = "Media Requests";
+                  cache = "20m";
+                  options = {
+                    base-url = "https://jellyseerr.iatze.cc";
+                    api-key = "\${JELLYSEERR_API_KEY}";
+                    limit = 20;
+                    collapse-after = 5;
+                  };
+                  template = ''
+                    {{ $baseURL := .Options.StringOr "base-url" "" }}
+                    {{ $apiKey := .Options.StringOr "api-key" "" }}
+
+                    {{ define "errorMsg" }}
+                      <div class="widget-error-header">
+                        <div class="color-negative size-h3">ERROR</div>
+                        <svg class="widget-error-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"></path>
+                        </svg>
+                      </div>
+                      <p class="break-all">{{ . }}</p>
+                    {{ end }}
+
+                    {{ if or
+                      (eq $baseURL "")
+                      (eq $apiKey "")
+                    }}
+                      {{ template "errorMsg" "Some required options are not set" }}
+                    {{ else }}
+
+                    {{ $limit := .Options.IntOr "limit" 20 }}
+                    {{ $collapseAfter := .Options.IntOr "collapse-after" 5 }}
+                    {{ $requestUrl := printf "%s/api/v1/request?take=%d&sort=added&sortDirection=desc" $baseURL $limit}}
+                    {{ $responseCall := "" }}
+                    {{ $response := "" }}
+
+                    <style>
+                      .media-requests-thumbnail {
+                        width: 5rem;
+                        aspect-ratio: 3 / 4;
+                        border-radius: var(--border-radius);
+                      }
+                      .color-purple { color: hsl(267deg, 84%, 81%); }
+                      .color-yellow { color: hsl(41deg, 86%, 83%); }
+                      .color-blue   { color: hsl(217deg, 92%, 76%); }
+                    </style>
+
+                    {{ $responseCall = newRequest $requestUrl
+                      | withHeader "X-Api-Key" $apiKey
+                      | withHeader "Accept" "application/json"
+                      | getResponse }}
+
+                    {{ if ne $responseCall.Response.StatusCode 200 }}
+                      {{ template "errorMsg" "Could not fetch Jellyseerr API." }}
+                    {{ else }}
+                      {{ $response = $responseCall.JSON.Array "results" }}
+                      <ul class="list list-gap-14 collapsible-container" data-collapse-after="{{ $collapseAfter }}">
+                          {{ range $response }}
+                            {{ $id := .Int "id" }}
+                            {{ $status := .Int "status" }}
+                            {{ $type := .String "type" }}
+
+                            {{ $mediaId := .Int "media.id" }}
+                            {{ $mediaTmdbId := .Int "media.tmdbId" }}
+                            {{ $mediaStatus := .Int "media.status" }}
+                            {{ $mediaType := .String "media.mediaType" }}
+
+                            {{ $mediaLink := printf "%s/%s/%d" $baseURL $type $mediaTmdbId }}
+
+                            {{ $userDisplayName := .String "requestedBy.displayName" }}
+                            {{ $userId := .Int "requestedBy.id" }}
+                            {{ $userAvatar := .String "requestedBy.Avatar" }}
+                            {{ $userLink := printf "%s/users/%d" $baseURL $userId }}
+
+                            {{ $itemName := "" }}
+                            {{ $itemBackdropPath := "" }}
+                            {{ $itemPosterPath := "" }}
+                            {{ $itemAirDate := "" }}
+                            {{ $itemInfoUrl := printf "%s/api/v1/%s/%d" $baseURL $mediaType $mediaTmdbId }}
+
+                            {{ $infoCall := newRequest $itemInfoUrl
+                              | withHeader "X-Api-Key" $apiKey
+                              | withHeader "Accept" "application/json"
+                              | getResponse }}
+
+                            {{ if eq $infoCall.Response.StatusCode 200 }}
+                              {{ $itemName = $infoCall.JSON.String "name" }}
+                              {{ $itemBackdropPath = $infoCall.JSON.String "backdropPath" }}
+                              {{ $itemPosterPath = $infoCall.JSON.String "posterPath" }}
+                              {{ if eq $mediaType "tv" }}
+                                  {{ $itemAirDate = $infoCall.JSON.String "firstAirDate" }}
+                              {{ else }}
+                                  {{ $itemAirDate = $infoCall.JSON.String "releaseDate" }}
+                              {{ end }}
+                            {{ end }}
+
+                            {{ $posterImageUrl := concat "https://image.tmdb.org/t/p/w600_and_h900_bestv2/" $itemPosterPath }}
+
+                            <li class="media-requests thumbnail-parent">
+                                <div class="flex gap-10 items-start">
+                                    <img class="media-requests-thumbnail thumbnail" loading="lazy" src="{{ $posterImageUrl }}" alt="">
+                                    <div class="min-width-0">
+                                        <a class="title size-h3 color-highlight text-truncate block" href="{{ $mediaLink }}" target="_blank" rel="noreferrer" title="{{ $itemName }}">
+                                            {{ $itemName }}
+                                        </a>
+                                        <ul class="list-horizontal-text">
+                                            {{ if eq $mediaStatus 5 }}
+                                            <li class="color-positive">Available</li>
+                                            {{ else if eq $mediaStatus 4 }}
+                                            <li class="color-yellow">Partial</li>
+                                            {{ else if eq $mediaStatus 3 }}
+                                            <li class="color-blue">Processing</li>
+                                            {{ else if eq $mediaStatus 2 }}
+                                            <li class="color-purple">Pending Approval</li>
+                                            {{ else }}
+                                            <li class="color-negative">Unknown</li>
+                                            {{ end }}
+                                        </ul>
+                                        <ul class="list-horizontal-text flex-nowrap">
+                                            <li>{{ $itemAirDate }}</li>
+                                            <li class="min-width-0">
+                                                <a class="color-primary" href="{{ $userLink }}" target="_blank" rel="noreferrer">{{ $userDisplayName }}</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </li>
+                          {{ end }}
+                      </ul>
+                    {{ end }}
+
+                    {{ end }}
+                  '';
+                }
               ];
             }
             {
