fe4723dbb591d64d9678061c858df026bf1c1fa3
frahz <me@frahz.dev>
2025-07-27 12:38:35 -0700

    chore: remove insecure packages

--- modules/base/nixpkgs/default.nix
+++ modules/base/nixpkgs/default.nix
@@ -10,15 +10,7 @@ in
     config = {
       allowUnfree = true;
       # allowAliases = false;
-      # TODO: until https://github.com/NixOS/nixpkgs/issues/360592 is resolved
-      permittedInsecurePackages = [
-        "aspnetcore-runtime-6.0.36"
-        "aspnetcore-runtime-wrapped-6.0.36"
-        "dotnet-runtime-6.0.36"
-        "dotnet-runtime-wrapped-6.0.36"
-        "dotnet-sdk-6.0.428"
-        "dotnet-sdk-wrapped-6.0.428"
-      ];
+      permittedInsecurePackages = [ ];
     };
   };
 
