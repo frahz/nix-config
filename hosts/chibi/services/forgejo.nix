5ce71baf7694249e3769165e1e299f49b1a1a261
frahz <me@frahz.dev>
2025-07-29 00:37:50 -0700

    chibi: fix forgejo ui

--- hosts/chibi/services/forgejo.nix
+++ hosts/chibi/services/forgejo.nix
@@ -5,19 +5,12 @@
   ...
 }:
 let
-  inherit (lib.modules) mkAfter mkForce;
-  inherit (lib.strings) removePrefix removeSuffix;
+  inherit (lib.modules) mkForce;
 
   httpPort = 3200;
   sshPort = 2222;
   domain = "git.${config.homelab.domain}";
 
-  # stole from https://github.com/isabelroses/dotfiles/blob/main/modules/nixos/services/selfhosted/forgejo.nix
-  theme = pkgs.fetchzip {
-    url = "https://github.com/catppuccin/gitea/releases/download/v1.0.1/catppuccin-gitea.tar.gz";
-    hash = "sha256-et5luA3SI7iOcEIQ3CVIu0+eiLs8C/8mOitYlWQa/uI=";
-    stripRoot = false;
-  };
 in
 {
   networking.firewall.allowedTCPPorts = [
@@ -25,16 +18,10 @@ in
     config.services.forgejo.settings.server.SSH_PORT
   ];
 
-  systemd.services.forgejo = {
-    preStart =
-      let
-        inherit (config.services.forgejo) stateDir;
-      in
-      mkAfter ''
-        rm -rf ${stateDir}/custom/public/assets
-        mkdir -p ${stateDir}/custom/public/assets
-        ln -sf ${theme} ${stateDir}/custom/public/assets/css
-      '';
+  catppuccin.forgejo = {
+    enable = true;
+    flavor = "mocha";
+    accent = "pink";
   };
 
   services.forgejo = {
@@ -59,16 +46,6 @@ in
         COOKIE_SECURE = true;
         # Sessions last for 1 weeks
         SESSION_LIFE_TIME = 86400 * 7;
-      };
-      ui = {
-        DEFAULT_THEME = "catppuccin-mocha-pink";
-        THEMES = builtins.concatStringsSep "," (
-          [ "auto,forgejo-auto,forgejo-dark,forgejo-light,arc-gree,gitea" ]
-          ++ (map (name: removePrefix "theme-" (removeSuffix ".css" name)) (
-            # IFD, https://github.com/catppuccin/nix/pull/179
-            builtins.attrNames (builtins.readDir theme)
-          ))
-        );
       };
     };
   };
