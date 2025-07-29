13de1a5f17bcf478403f27daacd01b7e1dc99446
frahz <me@frahz.dev>
2025-07-28 22:14:04 -0700

    chore: update fzf config

--- home/terminal/fzf.nix
+++ home/terminal/fzf.nix
@@ -1,8 +1,14 @@
-_: {
+{ lib, pkgs, ... }:
+let
+  find = "${lib.getExe pkgs.fd} --type=f --hidden --exclude=.git";
+in
+{
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
-    fileWidgetOptions = [ "--preview 'bat --color=always {}'" ];
+    defaultCommand = find;
+    fileWidgetCommand = find;
+    fileWidgetOptions = [ "--preview 'bat --plain --number --color=always {}'" ];
   };
   catppuccin.fzf.enable = true;
 }
