d96cca648ce133e49281f141f7524453eaf7b4cf
frahz <me@frahz.dev>
2025-07-29 00:13:28 -0700

    feat(direnv): setup direnv cache

--- home/terminal/direnv.nix
+++ home/terminal/direnv.nix
@@ -1,4 +1,4 @@
-_: {
+{
   programs.direnv = {
     enable = true;
     enableZshIntegration = true;
@@ -8,6 +8,20 @@ _: {
         hide_env_diff = true;
       };
     };
+
+    # store direnv in cache and not per project
+    # <https://github.com/direnv/direnv/wiki/Customizing-cache-location#hashed-directories>
+    stdlib = ''
+      : ''${XDG_CACHE_HOME:=$HOME/.cache}
+      declare -A direnv_layout_dirs
+
+      direnv_layout_dir() {
+        echo "''${direnv_layout_dirs[$PWD]:=$(
+          echo -n "$XDG_CACHE_HOME"/direnv/layouts/
+          echo -n "$PWD" | sha1sum | cut -d ' ' -f 1
+        )}"
+      }
+    '';
   };
   home.sessionVariables = {
     DIRENV_LOG_FORMAT = "";
