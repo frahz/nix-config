0acd331461faa05d4b056e1f18b728d42f404b52
frahz <me@frahz.dev>
2025-07-30 19:49:18 -0700

    feat(home/ssh): use matchBlocks

--- home/terminal/ssh.nix
+++ home/terminal/ssh.nix
@@ -1,25 +1,24 @@
-_: {
+{
   programs.ssh = {
     enable = true;
-    extraConfig = ''
-      Host chibi
-          Hostname 100.87.38.99
-          User frahz
-          IdentityFile ~/.ssh/id_ed25519_servers
-
-      Host inari
-          Hostname 100.68.202.4
-          User frahz
-          IdentityFile ~/.ssh/id_ed25519_servers
-
-      Host git.iatze.cc
-          Hostname 100.87.38.99
-          IdentityFile ~/.ssh/id_ed25519
-          Port 2222
-
-      Host github.com
-          Hostname github.com
-          IdentityFile ~/.ssh/id_ed25519
-    '';
+    matchBlocks = {
+      "chibi" = {
+        hostname = "100.87.38.99";
+        identityFile = "~/.ssh/id_ed25519_servers";
+      };
+      "inari" = {
+        hostname = "100.68.202.4";
+        identityFile = "~/.ssh/id_ed25519_servers";
+      };
+      "git.iatze.cc" = {
+        hostname = "100.87.38.99";
+        identityFile = "~/.ssh/id_ed25519";
+        port = 2222;
+      };
+      "github.com" = {
+        hostname = "github.com";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+    };
   };
 }
