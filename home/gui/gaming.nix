ebc03c4216f26f058473d479ab7d23f584a6d278
frahz <me@frahz.dev>
2025-08-02 17:11:38 -0700

    fix(home): graphical system config

--- home/gui/gaming.nix
+++ home/gui/gaming.nix
@@ -1,20 +1,33 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    legendary-gl # epic games launcher
-    mangohud # fps counter & vulkan overlay
-    (lutris.override {
-      extraPkgs = pkgs: [
-        winetricks
-        wineWowPackages.full
-      ];
-    }) # alternative game launcher
+  lib,
+  pkgs,
+  osConfig,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (pkgs.stdenv.hostPlatform) isLinux;
 
-    # emulators
-    # dolphin-emu # general console
+  cfg = osConfig.casa;
+in
+{
+  config = mkIf (cfg.profiles.graphical.enable && isLinux) {
+    home.packages = with pkgs; [
+      legendary-gl # epic games launcher
+      mangohud # fps counter & vulkan overlay
+      (lutris.override {
+        extraPkgs = pkgs: [
+          winetricks
+          wineWowPackages.full
+        ];
+      }) # alternative game launcher
 
-    # runtime
-    # mono # general dotnet apps
-    winetricks # wine helper utility
-  ];
+      # emulators
+      # dolphin-emu # general console
+
+      # runtime
+      # mono # general dotnet apps
+      winetricks # wine helper utility
+    ];
+  };
 }
--- home/services/hyprpaper.nix
+++ home/services/hyprpaper.nix
@@ -1,21 +1,34 @@
-{ osConfig, pkgs, ... }:
 {
-  home.packages = [ pkgs.hyprpaper ];
+  lib,
+  pkgs,
+  osConfig,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (pkgs.stdenv.hostPlatform) isLinux;
 
-  services.hyprpaper = {
-    inherit (osConfig.casa.profiles.graphical) enable;
-    settings = {
-      ipc = true;
-      splash = false;
-      preload = [
-        "~/Pictures/wl.jpg"
-        "~/Pictures/wl2.png"
-        "~/Pictures/GP2qRG5bAAAuXOa.jpg"
-        "~/Pictures/camille-villanueva-zsAPsRjzXRI-unsplash.jpg"
-      ];
-      wallpaper = [
-        "HDMI-A-1,~/Pictures/camille-villanueva-zsAPsRjzXRI-unsplash.jpg"
-      ];
+  cfg = osConfig.casa;
+in
+{
+  config = mkIf (cfg.profiles.graphical.enable && isLinux) {
+    home.packages = [ pkgs.hyprpaper ];
+
+    services.hyprpaper = {
+      enable = true;
+      settings = {
+        ipc = true;
+        splash = false;
+        preload = [
+          "~/Pictures/wl.jpg"
+          "~/Pictures/wl2.png"
+          "~/Pictures/GP2qRG5bAAAuXOa.jpg"
+          "~/Pictures/camille-villanueva-zsAPsRjzXRI-unsplash.jpg"
+        ];
+        wallpaper = [
+          "HDMI-A-1,~/Pictures/camille-villanueva-zsAPsRjzXRI-unsplash.jpg"
+        ];
+      };
     };
   };
 }
