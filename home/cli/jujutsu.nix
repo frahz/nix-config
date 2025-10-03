7c2f1b2a346c848b35eb0b6547bf673509d61a3a
frahz <me@frahz.dev>
2025-10-03 11:48:14 -0700

    chore(jj): tweak default command

--- home/cli/jujutsu.nix
+++ home/cli/jujutsu.nix
@@ -34,12 +34,40 @@ in
       };
 
       ui = {
-        default-command = "status";
+        default-command = [
+          "log"
+          "--template=log_compact"
+        ];
         pager = "${pkgs.delta}/bin/delta";
       };
 
       template-aliases = {
         "format_short_id(id)" = "id.shortest()";
+        "log_compact" = ''
+          if(root,
+            format_root_commit(self),
+            label(if(current_working_copy, "working_copy"),
+              concat(
+                separate(" ",
+                  format_short_change_id_with_hidden_and_divergent_info(self),
+                  if(empty, label("empty", "(empty)")),
+                  if(description,
+                    description.first_line(),
+                    label(if(empty, "empty"), description_placeholder),
+                  ),
+                  bookmarks,
+                  tags,
+                  working_copies,
+                  if(git_head, label("git_head", "HEAD")),
+                  if(conflict, label("conflict", "conflict")),
+                  if(config("ui.show-cryptographic-signatures").as_boolean(),
+                    format_short_cryptographic_signature(signature)),
+                  if(!description && !empty, "\n" ++ diff.summary()),
+                ) ++ "\n",
+              ),
+            )
+          )
+        '';
       };
     };
   };
