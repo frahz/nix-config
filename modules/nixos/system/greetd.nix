758d74a4dd294ed0fad4ebed55cf4bd7d877e0bc
frahz <me@frahz.dev>
2025-12-06 14:46:27 -0800

    fix: greetd tty issue

--- modules/nixos/system/greetd.nix
+++ modules/nixos/system/greetd.nix
@@ -24,5 +24,16 @@ in
         default_session = tuigreet_session;
       };
     };
+
+    systemd.services.greetd.serviceConfig = {
+      Type = "idle";
+      StandardInput = "tty";
+      StandardOutput = "tty";
+      StandardError = "journal"; # Without this errors will spam on screen
+      # Without these bootlogs will spam on screen
+      TTYReset = true;
+      TTYVHangup = true;
+      TTYVTDisallocate = true;
+    };
   };
 }
