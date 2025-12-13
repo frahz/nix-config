c7bde129717f799a1eea09393506ceff1836c29b
frahz <me@frahz.dev>
2025-12-12 18:51:55 -0800

    desktop: hyprland rules

--- home/gui/hyprland/config/rules.nix
+++ home/gui/hyprland/config/rules.nix
@@ -1,38 +1,39 @@
-_:
+{ lib, ... }:
 let
+  inherit (lib) concatLists;
   pip = "class:^(firefox)$, title:^(Firefox|Picture-in-Picture)$";
+
+  mkFloat = app: [
+    "center(1), ${app}"
+    "float, ${app}"
+    "size 40% 60%, ${app}"
+  ];
 in
 {
   wayland.windowManager.hyprland.settings = {
-    windowrulev2 = [
-      "suppressevent maximize, class:.*" # You'll probably like this.
+    windowrulev2 = concatLists [
+      [
+        "suppressevent maximize, class:.*" # You'll probably like this.
 
-      # Firefox PiP
-      "float, ${pip}"
-      "pin, ${pip}"
-      "keepaspectratio, ${pip}"
+        # Firefox PiP
+        "float, ${pip}"
+        "pin, ${pip}"
+        "keepaspectratio, ${pip}"
 
-      # Jellyfin running in XWayland
-      "tile, class:^(Jellyfin Media Player)$"
+        # Jellyfin running in XWayland
+        "tile, class:^(Jellyfin Media Player)$"
 
-      "float, class:^(com.gabm.satty)$"
-      "size >40% >60%, class:^(com.gabm.satty)$"
-      "minsize 600 400, class:^(com.gabm.satty)$"
+        "float, class:^(com.gabm.satty)$"
+        "size >40% >60%, class:^(com.gabm.satty)$"
+        "minsize 600 400, class:^(com.gabm.satty)$"
 
-      "float, class:^(org.gnome.Calculator)$"
-      "float, title:^(mdrop)$"
+        "float, class:^(org.gnome.Calculator)$"
+        "float, title:^(mdrop)$"
+      ]
 
-      "center(1), initialTitle:(Open Files)"
-      "float, initialTitle:(Open Files)"
-      "size 40% 60%, initialTitle:(Open Files)"
-
-      "center(1), class:^(com.saivert.pwvucontrol)$"
-      "float, class:^(com.saivert.pwvucontrol)$"
-      "size 40% 60%, class:^(com.saivert.pwvucontrol)$"
-
-      "center(1), initialTitle:(Bluetooth Devices)"
-      "float, initialTitle:(Bluetooth Devices)"
-      "size 40% 60%, initialTitle:(Bluetooth Devices)"
+      (mkFloat "initialTitle:(Open Files)")
+      (mkFloat "class:(com.saivert.pwvucontrol)")
+      (mkFloat "initialTitle:(Bluetooth Devices)")
     ];
   };
 }
