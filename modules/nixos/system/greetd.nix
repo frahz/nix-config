d6ee8eb96aa2aa3ceaf36159f5294348771555ea
frahz <me@frahz.dev>
2025-12-28 11:54:03 -0800

    chore: cleanup greetd config

--- modules/nixos/system/greetd.nix
+++ modules/nixos/system/greetd.nix
@@ -6,34 +6,20 @@
 }:
 let
   inherit (lib) mkIf;
+
+  session = "Hyprland";
 in
 {
   config = mkIf config.casa.profiles.graphical.enable {
     services.greetd = {
       enable = true;
-      settings = rec {
-        tuigreet_session =
-          let
-            session = "Hyprland";
-            tuigreet = "${lib.getExe pkgs.tuigreet}";
-          in
-          {
-            command = "${tuigreet} --time --remember --cmd ${session}";
-            user = "greeter";
-          };
-        default_session = tuigreet_session;
+      useTextGreeter = true;
+      settings = {
+        default_session = {
+          user = "greeter";
+          command = "${lib.getExe pkgs.tuigreet} --time --remember --cmd ${session}";
+        };
       };
-    };
-
-    systemd.services.greetd.serviceConfig = {
-      Type = "idle";
-      StandardInput = "tty";
-      StandardOutput = "tty";
-      StandardError = "journal"; # Without this errors will spam on screen
-      # Without these bootlogs will spam on screen
-      TTYReset = true;
-      TTYVHangup = true;
-      TTYVTDisallocate = true;
     };
   };
 }
