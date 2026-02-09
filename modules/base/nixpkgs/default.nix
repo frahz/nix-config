0b7d2dc7ecc6db8ba6693fde685cdfd067b213ab
frahz <me@frahz.dev>
2026-02-08 19:31:00 -0800

    chore: wip

--- modules/base/nixpkgs/default.nix
+++ modules/base/nixpkgs/default.nix
@@ -6,6 +6,21 @@ in
   nixpkgs = {
     overlays = [
       overlay-local
+      (final: prev: {
+        python313Packages = prev.python313Packages.override {
+          overrides = pythonFinal: pythonPrev: {
+            universal-silabs-flasher = pythonPrev.universal-silabs-flasher.overridePythonAttrs (old: {
+              doCheck = false;
+            });
+            zigpy = pythonPrev.zigpy.overridePythonAttrs (old: {
+              doCheck = false;
+            });
+            bellows = pythonPrev.bellows.overridePythonAttrs (old: {
+              doCheck = false;
+            });
+          };
+        };
+      })
     ];
     config = {
       allowUnfree = true;
