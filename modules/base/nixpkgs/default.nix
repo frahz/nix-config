be2592872f0dc59c60a2a73321b2bd88c16d9e8d
frahz <me@frahz.dev>
2026-03-25 21:56:48 -0700

    chore: update flake.lock

--- modules/base/nixpkgs/default.nix
+++ modules/base/nixpkgs/default.nix
@@ -7,19 +7,25 @@ in
     overlays = [
       overlay-local
       (final: prev: {
-        python313Packages = prev.python313Packages.override {
-          overrides = pythonFinal: pythonPrev: {
-            universal-silabs-flasher = pythonPrev.universal-silabs-flasher.overridePythonAttrs (old: {
-              doCheck = false;
+        pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+          (pythonFinal: pythonPrev: {
+            looptime = pythonPrev.looptime.overridePythonAttrs (old: {
+              disabledTestPaths = [
+                # Time-based tests that fail pretty predictably on darwin but pass on linux
+                "tests/test_chronometers.py::test_duration_resets_on_reuse"
+                "tests/test_chronometers.py::test_conversion_to_float"
+                "tests/test_chronometers.py::test_sync_context_manager"
+                "tests/test_chronometers.py::test_async_context_manager"
+                "tests/test_plugin.py::test_fixture_chronometer"
+                "tests/test_time_moves.py::test_real_time_is_ignored"
+                "tests/test_time_on_executors.py::test_with_sleep"
+                "tests/test_time_on_io_idle.py::test_end_of_time"
+                "tests/test_time_on_io_idle.py::test_no_idle_configured"
+                "tests/test_time_on_io_idle.py::test_stepping_with_no_limit"
+              ];
             });
-            zigpy = pythonPrev.zigpy.overridePythonAttrs (old: {
-              doCheck = false;
-            });
-            bellows = pythonPrev.bellows.overridePythonAttrs (old: {
-              doCheck = false;
-            });
-          };
-        };
+          })
+        ];
       })
     ];
     config = {
