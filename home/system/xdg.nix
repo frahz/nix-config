6ae9c1f2ff83b7dea1967567f739e4da932679c1
frahz <me@frahz.dev>
2026-07-02 20:45:03 -0700

    chore(hm): session variables

--- home/system/xdg.nix
+++ home/system/xdg.nix
@@ -1,22 +1,44 @@
 {
+  config,
   osConfig,
   ...
 }:
 let
 
-  cfg = osConfig.casa;
+  graphical = osConfig.casa.profiles.graphical;
 in
 {
   xdg = {
-    inherit (cfg.profiles.graphical) enable;
-    mime.enable = cfg.profiles.graphical.enable;
+    inherit (graphical) enable;
+    mime.enable = graphical.enable;
     mimeApps = {
-      inherit (cfg.profiles.graphical) enable;
+      inherit (graphical) enable;
       defaultApplications = {
         "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
         "x-scheme-handler/spotify" = [ "spotify.desktop" ];
         "x-scheme-handler/discord" = [ "vesktop.desktop" ];
       };
     };
+  };
+
+  home.sessionVariables = {
+    CODEX_HOME = "${config.xdg.configHome}/codex";
+    # Node.js configuration
+    # https://nodejs.org/api/repl.html#environment-variable-options
+    NODE_REPL_HISTORY = "${config.xdg.stateHome}/node_repl_history";
+    NPM_CONFIG_CACHE = "${config.xdg.cacheHome}/npm";
+    NPM_CONFIG_TMP = "$XDG_RUNTIME_DIR/npm";
+    NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/npm/config";
+  };
+
+  xdg.configFile = {
+    "npm/npmrc".text = ''
+      prefix=${config.xdg.dataHome}/npm
+      cache=${config.xdg.cacheHome}/npm
+      init-module=${config.xdg.configHome}/npm/config/npm-init.js
+    '';
+    "wget/wgetrc".text = ''
+      hsts-file = ${config.xdg.dataHome}/wget/hsts
+    '';
   };
 }
