d05f63b8cd37b0633c1d4f40efccfae843b74e65
frahz <me@frahz.dev>
2026-08-17 19:56:59 -0700

    chore: update flake.lock

--- modules/base/nixpkgs/default.nix
+++ modules/base/nixpkgs/default.nix
@@ -9,11 +9,11 @@ in
       (final: prev: {
         pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
           (pythonFinal: pythonPrev: {
-            nanoemoji = pythonPrev.nanoemoji.overridePythonAttrs (old: {
-              src = old.src.override {
-                hash = "sha256-FysyKC01XBnRiur5RR9fcsTxQqE8x0JJHSoe3q6JtKc=";
-              };
-            });
+            # nanoemoji = pythonPrev.nanoemoji.overridePythonAttrs (old: {
+            #   src = old.src.override {
+            #     hash = "sha256-FysyKC01XBnRiur5RR9fcsTxQqE8x0JJHSoe3q6JtKc=";
+            #   };
+            # });
           })
         ];
       })
